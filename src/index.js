7a59ee96d9e4c5826c098641a69796a988b981bc
herrmannplatz <rene@herrmannplatz.de>
2017-09-11 23:53:03 +0200

    add listeners only once

--- src/index.js
+++ src/index.js
@@ -7,32 +7,20 @@ const plugin = {
       }
     });
 
-    function handleOnline(e) { 
+    window.addEventListener('online', function handleOnline(e) { 
       vm.$data.online = true;
-    }
-
-    function handleOffline(e) {
+    });
+    
+    window.addEventListener('offline', function handleOffline(e) {
       vm.$data.online = false;
-    }
-
-    function addListeners() {
-      window.addEventListener('offline', handleOffline);
-      window.addEventListener('online', handleOnline);
-    }
-
-    function removeListeners() {
-      window.removeEventListener('offline', handleOffline);
-      window.removeEventListener('online', handleOnline);
-    }
+    });
 
     Vue.mixin({
       computed: {
         isOnline() {
           return vm.$data.online;
         }
-      },
-      beforeCreate: addListeners,
-      beforeDestroy: removeListeners
+      }
     });
   }
 };
